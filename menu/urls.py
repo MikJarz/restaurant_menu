9a65f8895c0ee06d91b83ab700c84f72fdca59d9
Mikołaj Jarząbkowski <mik.jarz00@gmail.com>
2020-08-28 10:40:36 +0200

    Added views to create/edit menus inside the site.

--- menu/urls.py
+++ menu/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.menu_list, name='menu_list'),
     path('menu/<int:pk>/', views.menu_detail, name='menu_detail'),
-
+    path('menu/new/', views.menu_new, name='menu_new'),
+    path('menu/<int:pk>/edit/', views.menu_edit, name='menu_edit'),
 ]
--- menu/views.py
+++ menu/views.py
@@ -1,13 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.utils import timezone
 from .models import Menu
 from django.shortcuts import render, get_object_or_404
+from .forms import MenuForm
 
 
 def menu_list(request):
     menus = Menu.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
     return render(request, 'menu/menu_list.html', {'menus': menus})
 
+
 def menu_detail(request, pk):
     menu = get_object_or_404(Menu, pk=pk)
     return render(request, 'menu/menu_detail.html', {'menu': menu})
+
+
+def menu_new(request):
+    if request.method == "POST":
+        form = MenuForm(request.POST)
+        if form.is_valid():
+            menu = form.save(commit=False)
+            menu.created_date = timezone.now()
+            menu.save()
+            return redirect('menu_detail', pk=menu.pk)
+    else:
+        form = MenuForm()
+    return render(request, 'menu/menu_edit.html', {'form': form})
+
+def menu_edit(request, pk):
+    menu = get_object_or_404(Menu, pk=pk)
+    if request.method == "POST":
+        form = MenuForm(request.POST, instance=menu)
+        if form.is_valid():
+            menu = form.save(commit=False)
+            menu.created_date = timezone.now()
+            menu.save()
+            return redirect('menu_detail', pk=menu.pk)
+    else:
+        form = MenuForm(instance=menu)
+    return render(request, 'menu/menu_edit.html', {'form': form})
